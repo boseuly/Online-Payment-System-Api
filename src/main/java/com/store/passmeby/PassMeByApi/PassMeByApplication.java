fdfd50bf5f2184af38ec1ce8bc6e2e9e03723c77
elena <tool0923@naver.com>
2025-12-16 16:49:29 +0900

    Initial commit

---
+++ src/main/java/com/store/passmeby/PassMeByApi/PassMeByApplication.java
@@ -0,0 +1,14 @@
+package com.store.passmeby.PassMeByApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassMeByApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassMeByApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/store/passmeby/pass_me_by/PassMeByApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.passmeby.pass_me_by;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassMeByApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
